d8fc40ea04b8b488e5dd9e60cb308ad0773e4707
fernandocrts <fernand.crts@gmail.com>
2019-03-17 15:07:08 -0300

    Removendo header

--- src/main/java/com/invilliatest/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/invilliatest/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,11 +1,11 @@
 package com.invilliatest.rest.webservices.restfulwebservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -15,20 +15,10 @@ public class RestfulWebServicesApplication {
 	}
 	
 	@Bean
-	public MessageSource messageSource() {
-	    ReloadableResourceBundleMessageSource messageSource
-	      = new ReloadableResourceBundleMessageSource();
-	     
-	    messageSource.setBasename("classpath:messages");
-	    messageSource.setDefaultEncoding("UTF-8");
-	    return messageSource;
-	}
-	
-	@Bean
-	public LocalValidatorFactoryBean getValidator() {
-	    LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
-	    bean.setValidationMessageSource(messageSource());
-	    return bean;
+	public AcceptHeaderLocaleResolver getValidator() {
+	    AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+	    localeResolver.setDefaultLocale(Locale.US);
+	    return localeResolver;
 	}
 	
 }
--- src/main/java/com/invilliatest/rest/webservices/restfulwebservices/helloworld/HelloworldResource.java
+++ src/main/java/com/invilliatest/rest/webservices/restfulwebservices/helloworld/HelloworldResource.java
@@ -1,11 +1,9 @@
 package com.invilliatest.rest.webservices.restfulwebservices.helloworld;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +13,8 @@ public class HelloworldResource {
 	private MessageSource messageSource;
 	
 	@GetMapping(path = "/hello-world-internationalized")
-	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale){
-		System.out.println("Teste");
-		return messageSource.getMessage("good.morning.message", null, locale);
+	public String helloWorldInternationalized(){
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
 	
